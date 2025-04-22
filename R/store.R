f48240b7800729b64428ad508508d72d91e33d51
Dan Turkenkopf <dan.turkenkopf@brewers.com>
2025-04-22 12:01:16 -0400

    Move the scripts directly to www so Shiny server can serve it correctly

--- R/store.R
+++ R/store.R
@@ -1,10 +1,10 @@
 #' Set up a Shiny app to use ShinyLocalStorage
-#' 
+#'
 #' This must be called from within a Shiny app's UI.
-#' 
+#'
 #' The ShinyLocalStorage Javascript library and dependencies
 #' will be inserted as script tags in the page header.
-#' 
+#'
 #' @examples
 #' if (interactive()) {
 #'   library(shiny)
@@ -25,25 +25,24 @@
 #' }
 #' @export
 useShinyLocalStorage <- function() {
-    template.loc <- file.path(find.package(package = .packageName), "scripts")
 
     htmltools::htmlDependency("shinyLocalStorage",
         "0.1.0",
-        src = template.loc,
+        src = "www",
         script = c("shinyLocalStorage.js", "localforage.min.js")
     )
 }
 
 #' Configures the ShinyLocalStorage instance for a given page / app.
-#' 
-#' This must be called from within the server function of a Shiny app. 
+#'
+#' This must be called from within the server function of a Shiny app.
 #' You are not required to configure ShinyLocalStorage, but it is recommended
 #' to get a unique data store for each app.
-#' 
+#'
 #' @param appName A unique identifier for the app. This is used to ensure that
 #'   the ShinyLocalStorage instance is unique to this app.
 #' @param session The Shiny session object
-#' @examples 
+#' @examples
 #' if (interactive()) {
 #'   library(shiny)
 #'
@@ -72,12 +71,12 @@ configureShinyLocalStorage <- function(appName, session = getDefaultReactiveDoma
 }
 
 #' Stores an item into the local storage.
-#' 
+#'
 #' @param key The key to store the value under
 #' @param value The value to store
 #' @param session The Shiny session object
-#' 
-#' @examples 
+#'
+#' @examples
 #' if(interactive()) {
 #'  observeEvent(input$btn, {
 #'     # Store the value into the local storage
@@ -91,20 +90,20 @@ store <- function(key, value, session = getDefaultReactiveDomain()) {
 
 
 #' Retrieves an item from local storage.
-#' 
+#'
 #' Stores the retrieved value into the Shiny \code{input} object
 #' named using a random prefix and the key, as well as returns it.
-#' 
+#'
 #' If \code{configureShinyLocalStorage} has not been called,
 #' the value will be stored in the \code{input} object under the key.
-#' 
+#'
 #' @param key The key to store the value under
 #' @param value The value to store
 #' @param session The Shiny session object
-#' 
+#'
 #' @return The value stored under the key
-#' 
-#' @examples 
+#'
+#' @examples
 #' if(interactive()) {
 #'  observeEvent(input$btn, {
 #'     # Store the value into the local storage
@@ -120,13 +119,13 @@ retrieve <- function(key, session = getDefaultReactiveDomain()) {
 }
 
 #' Clears all items from the local storage.
-#' 
+#'
 #' @param session The Shiny session object
-#' 
-#' @examples 
+#'
+#' @examples
 #' if(interactive()) {
 #'  observeEvent(input$clear, {
-#'   clear() 
+#'   clear()
 #'  })
 #' }
 #' @export
@@ -135,18 +134,18 @@ clear <- function(session = getDefaultReactiveDomain()) {
 }
 
 #' Removes a value from local storage.
-#' 
+#'
 #' Note: the namespaced \code{input} object will still exist.
-#' 
+#'
 #' @param key The key of the value to remove
 #' @param session The Shiny session object
-#' 
-#' @examples 
+#'
+#' @examples
 #' if(interactive()) {
 #'  observeEvent(input$remove, {
-#'    remove("stored_element") 
+#'    remove("stored_element")
 #'  })
-#' } 
+#' }
 #' @export
 remove <- function(key, session = getDefaultReactiveDomain()) {
     session$sendCustomMessage("remove", list(key = key))
